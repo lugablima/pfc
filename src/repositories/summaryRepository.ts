312adf831f573359a1e08a718ad3506dbb4678f2
Lucas <lugablima@gmail.com>
2024-01-10 22:00:16 -0300

    feat: removes Video and Summary services and repositories

--- src/repositories/summaryRepository.ts
+++ src/repositories/summaryRepository.ts
@@ -1,10 +1,2 @@
-import { prisma } from "../config/prisma";
-import { TSummary } from "../types/summaryTypes";
-
-export async function findOneByUrl(url: string): Promise<TSummary | null> {
-  const registeredSummary: TSummary | null = await prisma.summary.findUnique({
-    where: { url },
-  });
-
-  return registeredSummary;
-}
+// import { prisma } from "../config/prisma";
+// import { TSummary } from "../types/summaryTypes";
